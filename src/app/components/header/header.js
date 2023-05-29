196e8ce6b4a9f17ac2de84eef151f586026c9775
Vinit Gundeti <vinit@glideinvest.com>
2023-05-29 12:17:18 +0530

    header added

---
+++ src/app/components/header/header.js
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import style from "./header.module.css";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className={style.header}>
+      <Link href="/" title="Vinit Gundeti">
+        <Image
+          src="/assets/images/47896506.gif"
+          width={50}
+          height={50}
+          alt="Picture of the author"
+          className={style.headerImage}
+        />
+      </Link>
+      <nav>
+        <ul className={style.navList}>
+          <li>
+            <a className={style.links} title="About Me">About Me</a>
+          </li>
+          <li>
+            <a className={style.links} title="My Work">My Work</a>
+          </li>
+          <li>
+            <a className={style.links} title="My Skills">My Skills</a>
+          </li>
+          <li>
+            <a className={style.links} title="Testimonials">Testimonials</a>
+          </li>
+          <li>
+            <a className={style.links} title="Contact Me">Contact Me</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
